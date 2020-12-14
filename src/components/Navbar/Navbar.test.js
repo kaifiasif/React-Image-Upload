5c7258a3ee2c91340d7aad1e24f343d99e746c24
Kaifi Asif <kaifiasif@Saqlains-MacBook-Pro.local>
2020-12-14 14:26:58 +0530

    fixed typo error

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import NavbarItem from './NavbarItem';
-import Navbar from '../Navbar Compoment/NavbarItem'
+import Navbar from '../Navbar/NavbarItem'
 configure({ adapter: new Adapter() });
 describe('<Navbar/>', () => {
   it('should render correctly Navbar component', () => {  
